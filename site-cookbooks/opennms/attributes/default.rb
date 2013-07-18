e8c93836c0cb248308f7a6c1e5adc978d29a4a44
Ronny Trommer <ronny@opennms.org>
2013-07-18 09:45:08 +0200

    Verified pg_hba.conf on centos 6 and added default package name for Java OpenJDK 7 development kit on centos 6

--- site-cookbooks/opennms/attributes/default.rb
+++ site-cookbooks/opennms/attributes/default.rb
@@ -1,8 +1,8 @@
 case platform
 when "redhat", "centos", "scientific", "fedora", "suse", "amazon", "oracle"
-  default[:opennms][:jdk_package] = '' # TODO: Need to find default package name
+  default[:opennms][:jdk_package] = 'java-1.7.0-openjdk-devel'
   default[:opennms][:opennms_home] = '/opt/opennms'
-  default[:opennms][:pg_hba_location] = '/var/lib/postgresql/9.1/main/pg_hba.conf' # TODO: Need to validate location
+  default[:opennms][:pg_hba_location] = '/var/lib/pgsql/data/pg_hba.conf'
 when "debian", "ubuntu"
   default[:opennms][:jdk_package] = 'openjdk-7-jdk'
   default[:opennms][:opennms_home] = '/usr/share/opennms'
